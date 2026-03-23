ed97dc6145eb61ae208b9477a672305630d82b59
Andrii <andreiandreik19@gmail.com>
2026-03-23 20:17:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let headphonesQty = 0;
+let keyboardQty = 0;
+let mouseQty = 0;
+
 let count = 0;
 let total = 0;
 
@@ -7,28 +11,84 @@ const removeButtons = document.querySelectorAll(".remove-btn");
 const cartCount = document.getElementById("cart-count");
 const cartTotal = document.getElementById("cart-total");
 
+const headphonesQtyEl = document.getElementById("headphones-qty");
+const keyboardQtyEl = document.getElementById("keyboard-qty");
+const mouseQtyEl = document.getElementById("mouse-qty");
+
 addButtons.forEach(button => {
     button.addEventListener("click", () => {
         const price = Number(button.dataset.price);
-        
-        count++;
+        const product = button.dataset.product;
+
+        if (product === "headphones") {
+            headphonesQty++;
+        }
+
+        if (product === "keyboard") {
+            keyboardQty++;
+        }
+
+        if (product === "mouse") {
+            mouseQty++;
+        }
+
         total += price;
-        
+        count = headphonesQty + keyboardQty + mouseQty;
+
         cartCount.textContent = "Items: " + count;
         cartTotal.textContent = "Total: $" + total;
+
+        headphonesQtyEl.textContent = headphonesQty;
+        keyboardQtyEl.textContent = keyboardQty;
+        mouseQtyEl.textContent = mouseQty;
     });
 });
 
 removeButtons.forEach(button => {
     button.addEventListener("click", () => {
         const price = Number(button.dataset.price);
+        const product = button.dataset.product;
 
-        if (count > 0) {
-            count --;
+        if (product === "headphones" && headphonesQty > 0) {
+            headphonesQty--;
             total -= price;
-            
-            cartCount.textContent = "Items: " + count;
-            cartTotal.textContent = "Total: $" + total;
         }
+
+        if (product === "keyboard" && keyboardQty > 0) {
+            keyboardQty--;
+            total -= price;
+        }
+
+        if (product === "mouse" && mouseQty > 0) {
+            mouseQty--;
+            total -= price;
+        }
+
+        count = headphonesQty + keyboardQty + mouseQty;
+
+        cartCount.textContent = "Items: " + count;
+        cartTotal.textContent = "Total: $" + total;
+
+        headphonesQtyEl.textContent = headphonesQty;
+        keyboardQtyEl.textContent = keyboardQty;
+        mouseQtyEl.textContent = mouseQty;
     });
+});
+
+const clearCartBtn = document.getElementById("clear-cart");
+
+clearCartBtn.addEventListener("click", () => {
+    headphonesQty = 0;
+    keyboardQty = 0;
+    mouseQty = 0;
+
+    count = 0;
+    total = 0;
+
+    cartCount.textContent = "Items: 0";
+    cartTotal.textContent = "Total: $0";
+
+    headphonesQtyEl.textContent = 0;
+    keyboardQtyEl.textContent = 0;
+    mouseQtyEl.textContent = 0;
 });
